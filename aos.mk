f287c4076b22b37342808f4e61fd02e5c3c3a364
Shekhawat2 <anandsingh215@yahoo.com>
2017-07-22 23:04:31 +0530

    AOS
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- aos.mk
+++ aos.mk
@@ -14,13 +14,13 @@
 
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+# Inherit some commonAOS stuff.
+$(call inherit-product, vendor/aos/config/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
-PRODUCT_NAME := lineage_tomato
+PRODUCT_NAME := aos_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
