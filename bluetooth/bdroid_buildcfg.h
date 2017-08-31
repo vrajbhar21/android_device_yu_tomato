757cf0ce66e080cec60a4eb135c7989309d3f877
Shekhawat2 <anandsingh215@yahoo.com>
2017-08-31 14:59:31 +0530

    Revert "tomato: Clean up bluetooth config"
    This reverts commit 0676ffc516107e97a82fbbc519227688cac1b9d7.
    
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,5 +19,11 @@
 
 #define BTM_DEF_LOCAL_NAME   "YUREKA"
 #define BLUETOOTH_QTI_SW TRUE
-#define BLE_VND_INCLUDED   TRUE
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
+#define MAX_ACL_CONNECTIONS    7
+#define MAX_L2CAP_CHANNELS    16
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  TRUE
+#define BLE_PERIPHERAL_ADV_NAME  TRUE
 #endif
