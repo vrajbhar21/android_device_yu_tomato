df2b510a539665d8d0ac1e30b2dd7ea0a650772e
Shekhawat2 <anandsingh215@yahoo.com>
2017-09-04 23:51:47 +0530

    Add Lawnchair
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- device.mk
+++ device.mk
@@ -32,7 +32,8 @@ $(call inherit-product, vendor/yu/tomato/tomato-vendor.mk)
 PRODUCT_PACKAGES +=\
 MiXplorer \
 Phonograph \
-via
+via \
+lawnchair
 
 PRODUCT_PROPERTY_OVERRIDES += \
 pm.sleep_mode=1 \
