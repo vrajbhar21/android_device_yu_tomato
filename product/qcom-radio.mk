c60ab017abd7261042d703c8116876ee1d731e28
luca020400 <luca.stefani.ge1@gmail.com>
2017-10-13 12:05:11 +0530

    ril: add rild socket shim
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -15,3 +15,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+
+PRODUCT_PACKAGES += \
+    rild_socket
