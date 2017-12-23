05d9c29317b6668f66dc231b6ed0b5e63ec170f7
VIKAS RAJBHAR <mehul2363@gmail.com>
2017-12-23 16:36:30 +0530

    Update kernel.mk

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,3 +2,5 @@
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
